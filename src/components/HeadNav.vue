833cb8cdc1a8b5fa7101994f22c74f4330a65cd7
Mr.King <825551308@qq.com>
2022-10-25 17:08:22 +0800

    更新head nav 20221025 1639

--- src/components/HeadNav.vue
+++ src/components/HeadNav.vue
@@ -23,35 +23,9 @@
     <div class="boxDiv" v-show="boxShow" ref="boxRef">
       <!--   个人信息   -->
       <label @click="userInfoClick"> 个人信息 </label>
-
       <!--   退出登录   -->
       <label @click="unLoginClick" style="margin-top: 10px"> 退出登录 </label>
     </div>
-    <!--    <el-page-header @back="goBack" content="xxx" style="margin-left: 20px;">-->
-    <!--      &lt;!&ndash; Line 1 &ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #breadcrumb />&ndash;&gt;-->
-    <!--      &lt;!&ndash; Line 2 &ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #icon />&ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #title />&ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #content />&ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #extra />&ndash;&gt;-->
-    <!--&lt;!&ndash;      &lt;!&ndash; Lines after 2 &ndash;&gt;&ndash;&gt;-->
-    <!--&lt;!&ndash;      <template #default />&ndash;&gt;-->
-    <!--    </el-page-header>-->
-
-    <!--    <el-pagination background layout="pre,pager,next" hide-on-single-page>-->
-    <!--    </el-pagination>-->
-
-    <el-breadcrumb separator="/" :hidden="navHidden">
-      <!-- 自定义返回函数 -->
-      <el-breadcrumb-item @click="goback"> ← 返回</el-breadcrumb-item>
-      <!--item.meta.title  显示的还是路由里面的名字 -->
-      <el-breadcrumb-item v-for="item in navArray" :key="item.key">{{
-        // item.meta.title
-        //v-for="(item, index) in navArray
-        item.name
-      }}</el-breadcrumb-item>
-    </el-breadcrumb>
   </div>
 </template>
 
@@ -69,49 +43,43 @@ let userInfoDivRef = ref(null);
 let boxRight = ref("20px"); //box组件距离右边的值
 const route = useRoute();
 
-// 监听函数监听路径的变化
-watch(
-  () => route.path,
-  (newValue, oldValue) => {
-    // route.matched 目前匹配的是哪个 获得的值赋给  navArray.valu 上面用 v-for遍历值
-    navArray.value = route.matched;
-    console.log("newPath: " + newValue + " oldPath: " + oldValue);
-    // if(navArray.value.length <= 1) {
-    //   navHidden = true
-    //   console.log("navHidden: true  "+navArray.value.length)
-    // }else{
-    //   navHidden = false
-    //   console.log("navHidden: false   "+navArray.value.length)
-    // }
-  }
-);
-
-watch(
-  () => route.name,
-  (newValue, oldValue) => {
-    // route.matched 目前匹配的是哪个 获得的值赋给  navArray.valu 上面用 v-for遍历值
-    navArray.value = route.matched;
-    console.log("newValue: " + newValue + " oldValue: " + oldValue);
-  }
-);
-
-// function goBack() {
-//   console.log("后退->"+ history.length+" "+ router.all.length)
-//   // history.go(-1)
-//   router.back()
-// }
+// // 监听函数监听路径的变化
+// watch(
+//   () => route.path,
+//   (newValue, oldValue) => {
+//     // route.matched 目前匹配的是哪个 获得的值赋给  navArray.valu 上面用 v-for遍历值
+//     navArray.value = route.matched;
+//     console.log("newPath: " + newValue + " oldPath: " + oldValue);
+//     // if(navArray.value.length <= 1) {
+//     //   navHidden = true
+//     //   console.log("navHidden: true  "+navArray.value.length)
+//     // }else{
+//     //   navHidden = false
+//     //   console.log("navHidden: false   "+navArray.value.length)
+//     // }
+//   }
+// );
+//
+// watch(
+//   () => route.name,
+//   (newValue, oldValue) => {
+//     // route.matched 目前匹配的是哪个 获得的值赋给  navArray.valu 上面用 v-for遍历值
+//     navArray.value = route.matched;
+//     console.log("newValue: " + newValue + " oldValue: " + oldValue);
+//   }
+// );
 
 // 返回上一页，调用的组件 router.back();
-const goback = () => {
-  //点击了返回按钮
-  router.back();
-  console.log("后退后navArray个数->" + navArray.value.length);
-};
+// const goback = () => {
+//   //点击了返回按钮
+//   router.back();
+//   console.log("后退后navArray个数->" + navArray.value.length);
+// };
 
 // eslint-disable-next-line @typescript-eslint/no-empty-function,no-redeclare
 onMounted(function () {
   console.log("onMounted被执行");
-  // 加延迟是因为，直接获取到的userInfoDivRef宽度不对，可能是组件没有完全加载完
+  // 加延迟是因为，直接获取到的userInfoDiv宽度不对，可能是组件没有加载完全
   setTimeout(() => {
     // 计算box组件的right值
     // box.right = userInfoDiv 的宽度 + userInfoDiv.right - box.width * 0.5 - 头像宽度 * 0.5
@@ -122,6 +90,7 @@ onMounted(function () {
         boxRef.value.offsetWidth * 0.5 -
         30 * 0.5
       ).toString() + "px";
+    // 控制台 - 打印宽度
     console.log(
       "userInfoDiv宽度 -- " +
         userInfoDivRef.value.offsetWidth +
@@ -137,13 +106,11 @@ onMounted(function () {
 
 // 鼠标移动入到头像 ，显示展示列表
 function mouthInto(val) {
-  // console.log("鼠标 in");
   boxShow.value = true;
 }
 
 // 鼠标移出 ，隐藏展示列表
 function mouthLeave() {
-  // console.log("鼠标 out");
   boxShow.value = false;
 }
 
