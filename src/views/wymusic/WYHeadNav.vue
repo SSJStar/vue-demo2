49477c6a7085203ebd758b7fa84278f402bef33d
Mr.King <825551308@qq.com>
2022-12-06 17:08:43 +0800

    待修复：vform组，日历空间显示英文的问题 进行中： 1、正在写网易云音乐首页； 时间：20221206_1708

--- src/views/wymusic/WYHeadNav.vue
+++ src/views/wymusic/WYHeadNav.vue
@@ -1,60 +1,115 @@
 <template>
   <div class="wyhead-nav">
-    <!--    <div style="position: absolute; text-align: center">-->
     <!-- 网易云音乐 -->
-    <div style="background-color: #42b983; display: flex; align-items: center">
+    <div style="display: flex; align-items: center">
       <img
         src="@/assets/wymusic/wymusic_logo.png"
         style="width: 40px; height: 40px"
         alt=""
       />
-      <label>网易云音乐</label>
+      <label style="font-size: 24px">网易云音乐</label>
     </div>
+
     <!-- 其它菜单 -->
-    <!--    <div style="background-color: aquamarine">-->
-    <!--      <div v-for="{ item, index } in items" :key="index">-->
-    <!--        {{ item.message }}-->
-    <!--        &lt;!&ndash;        <label style="color: white"></label>&ndash;&gt;-->
-    <!--      </div>-->
-    <!--    </div>-->
+    <span style="width: 10px" />
+    <div class="otherMenuItem">
+      <div class="vFor" v-for="item in items" :key="item.index">
+        <div
+          :style="{
+            'background-color':
+              item.selected === true ? '#000000' : 'rgba(0, 0, 0, 0)',
+            height: '100%',
+            'align-items': 'center',
+            display: 'flex',
+          }"
+          @click="otherMenueClick(item.index)"
+        >
+          <label>
+            {{ item.message.toString() }}
+          </label>
+        </div>
+      </div>
+    </div>
+    <!-- hot图标 -->
+    <img
+      src="@/assets/wymusic/wymusic_hot.png"
+      style="width: 28px; height: 19px; margin-top: -20px"
+      alt=""
+    />
+
+    <!-- 搜索框 -->
+    <span style="width: 66px" /><!-- 70px间距 -->
+    <el-input
+      type="search"
+      style="width: 158px; border: none; border-radius: 30px"
+      placeholder="音乐/视频/电台/用户"
+      :prefix-icon="Search"
+    ></el-input>
+
+    <!-- 创作者中心 -->
+    <span style="width: 10px" /><!-- 30px间距 -->
+    <el-button
+      :style="{
+        'background-color': '#000',
+        color: '#ffffff',
+        'border-width': '1px',
+        'border-color': '#666',
+        'border-radius': '20px',
+      }"
+      >创作者中心</el-button
+    >
+
+    <!-- 登录 -->
+    <span style="width: 20px" /><!-- 40px间距 -->
+    <el-button style="background-color: unset; border-width: 0px; padding: 0"
+      >登录</el-button
+    >
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
-const items = ref([{ message: "Foo" }, { message: "Bar" }]);
-// 菜单 - 数组
-const navMenue = [
-  "发现音乐",
-  "我的音乐",
-  "关注",
-  "商城",
-  "音乐人",
-  "下载客户端",
-];
+import { ref } from "vue";
+import { Search } from "@element-plus/icons-vue";
+// import ssjSearch from "@/assets/wymusic/wymusic_search.png";
+// let searchIcon = require("../../assets/wymusic/wymusic_search.png");
 
-// let navHidden = ref(true); //控制返回控件的显示或隐藏（根据navArray个数）
-// let navArray = ref([]);
-// let nickNameVal = ref(""); //昵称
-// let boxShow = ref(true); //box显示与否,默认显示，目的是为了在onMounted里得到实际宽度
-// let boxRef = ref(null);
-// let userInfoDivRef = ref(null);
-// let boxRight = ref("20px"); //box组件距离右边的值
-// let mouthInBox = false; //当前光标是否在列表上
+// 菜单 - 数组
+const items = ref([
+  { index: "0", message: "发现音乐", selected: false },
+  { index: "1", message: "我的音乐", selected: false },
+  { index: "2", message: "关注", selected: false },
+  { index: "3", message: "商城", selected: false },
+  { index: "4", message: "音乐人", selected: false },
+  { index: "5", message: "下载客户端", selected: false },
+]);
 
 // 设置导航栏高度、背景颜色、字体大小、字体颜色
 let navgation_height = "70px";
 let navgation_bgColor = "#242424";
-let navgation_fontSize = "18";
+let navgation_fontSize = "16px";
 let navgation_titleColor = "#FFFFFF";
 
-onMounted(function () {
-  console.log("onMounted被执行");
-  // 加延迟是因为，直接获取到的userInfoDiv宽度不对，可能是组件没有加载完全
-  // setTimeout(() => {
-  //
-  // }, 0);
-}, undefined);
+// onMounted(function () {
+//   console.log("onMounted被执行");
+//   // 加延迟是因为，直接获取到的userInfoDiv宽度不对，可能是组件没有加载完全
+//   // setTimeout(() => {
+//   //
+//   // }, 0);
+// }, undefined);
+// 菜单 - 点击
+function otherMenueClick(indexParamter) {
+  console.log(`indexParamter--${indexParamter}`);
+  items.value.forEach((item, index) => {
+    console.log(`index--${index}`);
+    // let obj = item;
+    if (index.toString() === indexParamter.toString()) {
+      item.selected = true;
+      console.log(`找到了${index}`);
+    } else {
+      item.selected = false;
+    }
+  });
+}
 </script>
 
 <style>
@@ -74,37 +129,18 @@ onMounted(function () {
   color: v-bind(navgation_titleColor);
 }
 
-/* 头像 + 昵称 */
-/*.userInfoDiv {*/
-/*  right: 20px;*/
-/*  height: 40px;*/
-/*  position: absolute;*/
-/*  display: flex;*/
-/*  align-items: center;*/
-/*}*/
-/*.userInfoDiv img {*/
-/*  width: 30px;*/
-/*  height: 30px;*/
-/*  border-radius: 50%;*/
-/*}*/
-/*.userInfoDiv label {*/
-/*  float: right;*/
-/*  margin-left: 10px;*/
-/*  height: 30px;*/
-/*}*/
-
-/* 点击头像-列表 */
-/*.boxDiv {*/
-/*  display: flex;*/
-/*  top: 51px;*/
-/*  right: v-bind(boxRight);*/
-/*  background-color: #333333;*/
-/*  position: absolute;*/
-/*  flex-direction: column;*/
-/*  padding: 10px 8px;*/
-/*  z-index: 999;*/
-/*}*/
-/*.boxDiv label {*/
-/*  color: white;*/
-/*}*/
+.otherMenuItem {
+  display: flex;
+  height: 100%;
+}
+.otherMenuItem .vFor {
+  height: 100%;
+  align-items: center; /** 垂直居中 */
+  display: flex;
+}
+.otherMenuItem label {
+  color: white;
+  margin-left: 10px;
+  margin-right: 10px;
+}
 </style>
--- src/views/wymusic/WYMusicView.vue
+++ src/views/wymusic/WYMusicView.vue
@@ -1,6 +1,36 @@
 <template>
   <div class="WYMusicView">
     <WYHeadNav></WYHeadNav>
+    <!-- 跑马灯 -->
+    <el-carousel height="150px" style="padding-left: 10%; padding-right: 10%">
+      <el-carousel-item v-for="item in 4" :key="item">
+        <h3 class="small justify-center" text="2xl">{{ item }}</h3>
+      </el-carousel-item>
+    </el-carousel>
+
+    <!-- 音乐列表 -->
+    <div style="padding-left: 10%; padding-right: 10%">
+      <div style="display: flow">
+        <div class="leftView" style="float: left">
+          <img
+            src="@/assets/wymusic/wymusic_remen.png"
+            style="width: 20px; height: 20px"
+            alt=""
+          />
+          <span style="width: 10px" />
+          <label style="font-size: 18px">热门推荐</label>
+        </div>
+        <!-- 更多 -->
+        <div class="rightView" style="float: right">
+          更多
+          <img
+            src="@/assets/wymusic/wymusic_more.png"
+            style="width: 12px; height: 12px"
+            alt=""
+          />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -19,5 +49,12 @@ onMounted(() => {
   overflow: hidden;
   position: absolute; /*不加这句，高度不能铺满*/
   background-color: #999999;
+}
+
+.el-dropdown-link {
+  cursor: pointer;
+  color: var(--el-color-primary);
+  display: flex;
+  align-items: center;
 }
 </style>
